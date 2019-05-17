3d62b51398706d3994a5c7509eb512df1a9e664e
andri <andriiandrosiukk@gmail.com>
2019-05-18 00:04:58 +0200

    Initial commit

---
+++ src/main/java/pl/jarofitrainer/jarofit_trainer_app/JarofitTrainerAppApplication.java
@@ -0,0 +1,13 @@
+package pl.jarofitrainer.jarofit_trainer_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarofitTrainerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JarofitTrainerAppApplication.class, args);
+	}
+
+}
