9f9bb7a75020692cf20adbd233e01a72db4c2974
andri <andriiandrosiukk@gmail.com>
2019-05-20 22:09:22 +0200

    Rebuild program using jsp

--- src/main/java/pl/jarofitrainer/jarofit_trainer_app/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.jarofitrainer.jarofit_trainer_app.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import pl.jarofitrainer.jarofit_trainer_app.model.User;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-}
